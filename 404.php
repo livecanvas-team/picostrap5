2a9c26a64e9464604038b94b8268d4d3729e3457
Jeff <dopewp@gmail.com>
2023-04-19 12:57:43 +0200

    fix 404 logo

--- 404.php
+++ 404.php
@@ -17,7 +17,7 @@ get_header();
 
 
 	<div class="py-6 bg-transparent text-center w-100">
-		<svg xmlns="http://www.w3.org/2000/svg" id="fd59ce54-f850-4dfc-bc34-dd7d379d600e" data-name="Layer 1" width="150" height="auto" viewBox="0 0 1074.392 584.231" class="injected-svg modal__media modal__lg_media" data-src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/page_not_found_su7k.svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+		<svg xmlns="http://www.w3.org/2000/svg" id="fd59ce54-f850-4dfc-bc34-dd7d379d600e" data-name="Layer 1" width="156" height="100" viewBox="0 0 1074.392 584.231" class="injected-svg modal__media modal__lg_media" data-src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/page_not_found_su7k.svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 			
 			<ellipse cx="540.64346" cy="549.3094" rx="527.5" ry="34.9216" fill="#f2f2f2"></ellipse>
 			<path d="M583.47969,324.89424c-85.94407,0-147.651,55.13938-147.651,183.79791,0,145.813,61.70691,184.41057,147.651,184.41057s151.327-42.27352,151.327-184.41057C734.80664,356.75255,669.42376,324.89424,583.47969,324.89424Zm.56495,319.80837c-59.52686,0-90.62592-34.92288-90.62592-135.9163,0-89.11185,32.37209-136.10461,91.899-136.10461s91.899,30.86774,91.899,136.10461C677.21663,607.23367,643.5715,644.70261,584.04464,644.70261Z" transform="translate(-63.054 -157.8845)" fill="#2f2e41"></path>
