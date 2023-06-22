6e149388fa1b6a85146db493d8547f7021211e67
Jeff <dopewp@gmail.com>
2023-06-22 18:47:31 +0200

    convert picosass to async

--- inc/customizer-assets/picosass.js
+++ inc/customizer-assets/picosass.js
@@ -15,30 +15,8 @@ const replaceLast = (str, pattern, replacement) => {
         : str;
 };
 
-async function load(canonicalUrl) {
-    //console.log(`SASS: Importing ${canonicalUrl} (async)`);
-
-    //determine if we should force reading a locally-cached version of the file,  or force the download of a fresh copy of the requested scss file
-    const fetchDataArgs = canonicalUrl.pathname.includes('cache') ? {
-        cache: 'force-cache',
-        headers: {
-            'Cache-Control': 'max-age=3600',
-            'Pragma': 'max-age=3600', // added for redundancy
-        }
-    } : { cache: "no-cache" }
-
-    //fetch the URL and handle stuff
-    const response = await fetch(canonicalUrl, fetchDataArgs)
-    if (!response.ok) throw new Error(`Failed to fetch ${canonicalUrl}: ${response.status} (${response.statusText})`);
-    const contents = await response.text()
-    return {
-        contents,
-        syntax: canonicalUrl.pathname.endsWith('.sass') ? 'indented' : 'scss'
-    }
-}
 
 function canonicalize(url) {
-
     //console.log('canonicalize '+url);
 
     //if it's not the main file, or the main bs file, add underscores in front of scss file names
@@ -47,10 +25,36 @@ function canonicalize(url) {
     }
 
     //create URL object to be consumed by the compiler
-    return new URL(url + ".scss", window.location.toString())
+    return new URL(url+'.scss', window.location.toString())
+    
 }
 
-export async function Compile(sassParams = {  /* style: "compressed" */ }) {
+function load(canonicalUrl) {
+    console.log(`Importing ${canonicalUrl} (sync)`)
+    const request = new XMLHttpRequest();
+    if (  canonicalUrl.pathname.includes('cache')){
+        //force usage of a cache
+        request.setRequestHeader('Cache-Control', 'max-age=3600');
+    } else {
+        // disable browser caching in request header
+        //request.setRequestHeader('Cache-Control', 'no-cache, no-store, max-age=0');
+        //request.setRequestHeader('Expires', 'Thu, 1 Jan 1970 00:00:00 GMT');
+        //request.setRequestHeader('Pragma', 'no-cache');
+    }
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status !== 200)
+            throw new Error(`Failed to fetch ${canonicalUrl}: ${request.status} (${request.statusText})`);
+    };
+    request.open("GET", canonicalUrl, false);
+    request.send();
+    return {
+        contents: request.responseText,
+        syntax: canonicalUrl.pathname.endsWith('.sass') ? 'indented' : 'scss'
+    };
+}
+ 
+
+export function Compile(sassParams = {  /* style: "compressed" */ }) {
 
     //show feedback message: we are compiling ....
     document.querySelector("#picosass-output-feedback").innerHTML = " Compiling SCSS... ";
@@ -63,30 +67,32 @@ export async function Compile(sassParams = {  /* style: "compressed" */ }) {
     
     const theCode = document.querySelector("#the-scss").innerHTML;  
 
+    let compiled = "";
+
     //setup and compile  
     try {
 
-        const compiled = await sass.compileStringAsync(theCode, sassParams);
-
-        //console.log(compiled); 
-
-        //add the resulting CSS to the page 
-        document.querySelector('#picosass-injected-style').innerHTML = compiled.css;
-
-        //remove initial static CSS 
-        document.querySelector("#picostrap-styles-css")?.setAttribute("disabled", "true");
-
-        //as there are no errors, clear the output feedback
-        document.querySelector("#picosass-output-feedback").innerHTML = '';
-
-        //return for additional processing or saving 
-        return compiled.css; //cant we have a string man?
-
+        compiled = sass.compileString(theCode, sassParams);
+    
     } catch (err) {
 
         //show error in output feedback 
         document.querySelector("#picosass-output-feedback").innerHTML = err;
     }
+    
+    //console.log(compiled); 
+
+    //add the resulting CSS to the page 
+    document.querySelector('#picosass-injected-style').innerHTML = compiled.css;
+
+    //remove initial static CSS 
+    document.querySelector("#picostrap-styles-css")?.setAttribute("disabled", "true");
+
+    //as there are no errors, clear the output feedback
+    document.querySelector("#picosass-output-feedback").innerHTML = '';
+
+    //return for additional processing or saving 
+    return compiled.css; //cant we have a string ?
 
 }
 
@@ -101,8 +107,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     if (!document.querySelector("#picosass-output-feedback")) document.querySelector("html").insertAdjacentHTML("afterbegin", `<div id='picosass-output-feedback'></div> <style> #picosass-output-feedback { position: fixed; z-index: 99999999; font-size:30px; background:#212337; color:lime; font-family:courier; border:8px solid red; padding:15px; display:block; user-select: none; } #picosass-output-feedback:empty {display:none} </style> `);
 
     //run  the compiler, unless a special class is added to the body
-    if (!document.querySelector("body").classList.contains("prevent-sass-autocompile")) Compile();
-
+    if (!document.querySelector("body").classList.contains("prevent-sass-autocompile")) {
+        console.log("run compile");
+        Compile();
+    }
+    
     //attach observer to detect on-page scss code changes 
     //TODO: this is not working inside theme, yet
     let observer = new MutationObserver(mutationRecords => {
@@ -117,7 +126,4 @@ window.addEventListener("DOMContentLoaded", (event) => {
         characterDataOldValue: true // pass old data to callback
     });
     
-
-
-});
-
+}); //end onDOMContentLoaded
