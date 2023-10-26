0d9c0ed646596b95eb902db5c356cf054b368201
Giovanni Panasiti <giova.panasiti@gmail.com>
2023-10-26 13:13:49 +0200

    add release script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Prompt the user for a version number
+echo "Release picostrap5:"
+
+
+# Define a subfolder name
+SUBFOLDER="picostrap5"
+
+# Create the subfolder if it doesn't exist
+mkdir -p "$SUBFOLDER"
+
+# Copy the files into the subfolder
+cp -R . "$SUBFOLDER/"
+
+rm -rf "$SUBFOLDER"/.git
+rm -rf "$SUBFOLDER"/.gitignore
+rm -rf "$SUBFOLDER"/.DS_Store
+
+# Zip the subfolder
+zip -r "${SUBFOLDER}.zip" "$SUBFOLDER/"
+
+# Optionally, remove the subfolder after zipping
+rm -rf "$SUBFOLDER"
