c70c6e6eb1736507e88142d13134203832c19849
Jeff <dopewp@gmail.com>
2023-10-27 16:30:34 +0200

    topbar enabled body class

--- inc/customizer-assets/customizer.php
+++ inc/customizer-assets/customizer.php
@@ -55,6 +55,8 @@ function picostrap_config_body_classes( $classes ) {
 	
 	$classes[]="picostrap_header_navbar_position_".get_theme_mod('picostrap_header_navbar_position');
 	$classes[]="picostrap_header_navbar_color_choice_".get_theme_mod('picostrap_header_navbar_color_choice');
+
+    if (get_theme_mod("enable_topbar")) $classes[]="picostrap_topbar_enabled";
 	
 	return $classes;
 }
