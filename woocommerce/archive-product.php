eb46ef706a816855d1acc722ebaab39c97266ebc
Command <superthemes76@gmail.com>
2024-06-13 17:31:10 +0200

    ### Key Changes
    1. **Addition of the Hook `woocommerce_shop_loop_header`**: This hook has been added to follow the new modifications introduced in version 8.6.0.
    2. **Compatibility with the New Header and Breadcrumbs**: The new header and breadcrumbs are now included using the relevant hooks.
    3. **Sidebar Control**: The structure of the row and columns has been maintained to include the sidebar if it is active.
    4. **Updating Comments and Version**: Comments have been updated to reflect the new version of the template (8.6.0).

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -10,9 +10,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see https://docs.woocommerce.com/document/template-structure/
+ * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.4.0
+ * @version 8.6.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -28,85 +28,75 @@ get_header( 'shop' );
  */
 do_action( 'woocommerce_before_main_content' );
 
+/**
+ * Hook: woocommerce_shop_loop_header.
+ *
+ * @since 8.6.0
+ *
+ * @hooked woocommerce_product_taxonomy_archive_header - 10
+ */
+do_action( 'woocommerce_shop_loop_header' );
 ?>
-<header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
-
-	<?php
-	/**
-	 * Hook: woocommerce_archive_description.
-	 *
-	 * @hooked woocommerce_taxonomy_archive_description - 10
-	 * @hooked woocommerce_product_archive_description - 10
-	 */
-	do_action( 'woocommerce_archive_description' );
-	?>
-</header>
 <div class="row">
-	<div class="<?php if (is_active_sidebar( 'wc-sidebar' )) echo "col-md-9 order-2"; else echo "col"; ?>">
-		<?php
-		if ( woocommerce_product_loop() ) {
+    <div class="<?php if (is_active_sidebar( 'wc-sidebar' )) echo 'col-md-9 order-2'; else echo 'col'; ?>">
+        <?php
+        if ( woocommerce_product_loop() ) {
 
-			/**
-			 * Hook: woocommerce_before_shop_loop.
-			 *
-			 * @hooked woocommerce_output_all_notices - 10
-			 * @hooked woocommerce_result_count - 20
-			 * @hooked woocommerce_catalog_ordering - 30
-			 */
-			do_action( 'woocommerce_before_shop_loop' );
+            /**
+             * Hook: woocommerce_before_shop_loop.
+             *
+             * @hooked woocommerce_output_all_notices - 10
+             * @hooked woocommerce_result_count - 20
+             * @hooked woocommerce_catalog_ordering - 30
+             */
+            do_action( 'woocommerce_before_shop_loop' );
 
-			woocommerce_product_loop_start();
+            woocommerce_product_loop_start();
 
-			if ( wc_get_loop_prop( 'total' ) ) {
-				while ( have_posts() ) {
-					the_post();
+            if ( wc_get_loop_prop( 'total' ) ) {
+                while ( have_posts() ) {
+                    the_post();
 
-					/**
-					 * Hook: woocommerce_shop_loop.
-					 */
-					do_action( 'woocommerce_shop_loop' );
+                    /**
+                     * Hook: woocommerce_shop_loop.
+                     */
+                    do_action( 'woocommerce_shop_loop' );
 
-					wc_get_template_part( 'content', 'product' );
-				}
-			}
+                    wc_get_template_part( 'content', 'product' );
+                }
+            }
 
-			woocommerce_product_loop_end();
+            woocommerce_product_loop_end();
 
-			/**
-			 * Hook: woocommerce_after_shop_loop.
-			 *
-			 * @hooked woocommerce_pagination - 10
-			 */
-			do_action( 'woocommerce_after_shop_loop' );
-		} else {
-			/**
-			 * Hook: woocommerce_no_products_found.
-			 *
-			 * @hooked wc_no_products_found - 10
-			 */
-			do_action( 'woocommerce_no_products_found' );
-		}
-
-
-		?>
-	</div>
-	<?php if (is_active_sidebar( 'wc-sidebar' )): ?>
-		<div id = "store-sidebar" class="col-md-3 order-1 pt-1 mb-4">
-			<?php
-			/**
-			 * Hook: woocommerce_sidebar.
-			 *
-			 * @hooked woocommerce_get_sidebar - 10
-			 */
-			do_action( 'woocommerce_sidebar' );
-			?>
-		</div> <!-- close col -->
-	<?php endif ?>
+            /**
+             * Hook: woocommerce_after_shop_loop.
+             *
+             * @hooked woocommerce_pagination - 10
+             */
+            do_action( 'woocommerce_after_shop_loop' );
+        } else {
+            /**
+             * Hook: woocommerce_no_products_found.
+             *
+             * @hooked wc_no_products_found - 10
+             */
+            do_action( 'woocommerce_no_products_found' );
+        }
+        ?>
+    </div>
+    <?php if (is_active_sidebar( 'wc-sidebar' )): ?>
+        <div id="store-sidebar" class="col-md-3 order-1 pt-1 mb-4">
+            <?php
+            /**
+             * Hook: woocommerce_sidebar.
+             *
+             * @hooked woocommerce_get_sidebar - 10
+             */
+            do_action( 'woocommerce_sidebar' );
+            ?>
+        </div>
+    <?php endif; ?>
 </div>
-
 <?php
 /**
  * Hook: woocommerce_after_main_content.
@@ -116,3 +106,4 @@ do_action( 'woocommerce_before_main_content' );
 do_action( 'woocommerce_after_main_content' );
 
 get_footer( 'shop' );
+?>
