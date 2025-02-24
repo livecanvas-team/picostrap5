d136baabbc92ca3a50a2a240f9a827f5e405c95b
Command <superthemes76@gmail.com>
2025-02-24 22:56:12 +0100

    bg-light from sidebar wrapper has been removed

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <?php if (is_active_sidebar( 'main-sidebar' )): ?>
 
-    <div class="wrapper bg-light p-3" id="wrapper-main-sidebar">
+    <div class="wrapper p-3" id="wrapper-main-sidebar">
     
         <?php dynamic_sidebar( 'main-sidebar' ); ?>
     
--- woocommerce/global/sidebar.php
+++ woocommerce/global/sidebar.php
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if (!is_single() && is_active_sidebar( 'wc-sidebar' )): ?>
 
-    <div class="wrapper bg-light p-3" id="wrapper-main-sidebar-widgets">
+    <div class="wrapper p-3" id="wrapper-main-sidebar-widgets">
     
         <?php dynamic_sidebar( 'wc-sidebar' ); ?>
     
