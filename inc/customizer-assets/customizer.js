5b09541f3a8572f236e0ded44d8f24a69d4cd022
Jeff <dopewp@gmail.com>
2025-03-21 12:08:22 +0100

    Fix Bootstrap Logo in Customizer

--- inc/customizer-assets/customizer.js
+++ inc/customizer-assets/customizer.js
@@ -337,7 +337,37 @@
 
 	////////////////////////////////////////// DOCUMENT READY //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	$(document).ready(function() {
-			
+		
+        //ADD Bootstrap logo before accordion buttons in Customizer
+        const accordionTargets = [
+            'accordion-section-colors',
+            'accordion-section-typography',
+            'accordion-section-components',
+            'accordion-section-buttons-forms',
+            'accordion-section-buttons',
+        ];
+
+        accordionTargets.forEach(id => {
+            const accordionButton = document.querySelector(`#${id} h3.accordion-section-title button`);
+            if (accordionButton) {
+                const iconHTML = `
+                    <span style="
+                    display: inline;
+                    width: 1.3em;
+                    height: 1.3em;
+                    margin-right: 12px;
+                    margin-top:4px;
+                    opacity: 0.7;
+                    background-repeat: no-repeat;
+                    background-size: contain;
+                    background-image: url('data:image/svg+xml,%3Csvg xmlns=%27http://www.w3.org/2000/svg%27 width=%27512%27 height=%27408%27%3E%3Cdefs%3E%3ClinearGradient id=%27bs-logo-a%27 x1=%2776.079%27 x2=%27523.48%27 y1=%2710.798%27 y2=%27365.945%27 gradientUnits=%27userSpaceOnUse%27%3E%3Cstop stop-color=%27%239013fe%27/%3E%3Cstop offset=%271%27 stop-color=%27%236610f2%27/%3E%3C/linearGradient%3E%3ClinearGradient id=%27bs-logo-b%27 x1=%27193.508%27 x2=%27293.514%27 y1=%27109.74%27 y2=%27278.872%27 gradientUnits=%27userSpaceOnUse%27%3E%3Cstop stop-color=%27%23fff%27/%3E%3Cstop offset=%271%27 stop-color=%27%23f1e5fc%27/%3E%3C/linearGradient%3E%3Cfilter xmlns=%27http://www.w3.org/2000/svg%27 id=%27bs-logo-c%27 width=%27197%27 height=%27249%27 x=%27161.901%27 y=%2783.457%27 color-interpolation-filters=%27sRGB%27 filterUnits=%27userSpaceOnUse%27%3E%3CfeFlood flood-opacity=%270%27 result=%27BackgroundImageFix%27/%3E%3CfeColorMatrix in=%27SourceAlpha%27 values=%270 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0%27/%3E%3CfeOffset dy=%274%27/%3E%3CfeGaussianBlur stdDeviation=%278%27/%3E%3CfeColorMatrix values=%270 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0%27/%3E%3CfeBlend in2=%27BackgroundImageFix%27 result=%27effect1_dropShadow%27/%3E%3CfeBlend in=%27SourceGraphic%27 in2=%27effect1_dropShadow%27 result=%27shape%27/%3E%3C/filter%3E%3C/defs%3E%3Cpath fill=%27url(%23bs-logo-a)%27 d=%27M56.481 53.32C55.515 25.58 77.128 0 106.342 0h299.353c29.214 0 50.827 25.58 49.861 53.32-.928 26.647.277 61.165 8.964 89.31 8.715 28.232 23.411 46.077 47.48 48.37v26c-24.069 2.293-38.765 20.138-47.48 48.37-8.687 28.145-9.892 62.663-8.964 89.311.966 27.739-20.647 53.319-49.861 53.319H106.342c-29.214 0-50.827-25.58-49.86-53.319.927-26.648-.278-61.166-8.966-89.311C38.802 237.138 24.07 219.293 0 217v-26c24.069-2.293 38.802-20.138 47.516-48.37 8.688-28.145 9.893-62.663 8.965-89.31z%27/%3E%3Cpath fill=%27url(%23bs-logo-b)%27 filter=%27url(%23bs-logo-c)%27 stroke=%27%23fff%27 d=%27M267.103 312.457c47.297 0 75.798-23.158 75.798-61.355 0-28.873-20.336-49.776-50.532-53.085v-1.203c22.185-3.609 39.594-24.211 39.594-47.219 0-32.783-25.882-54.138-65.322-54.138h-88.74v217h89.202zm-54.692-189.48h45.911c24.958 0 39.131 11.128 39.131 31.279 0 21.505-16.484 33.535-46.372 33.535h-38.67v-64.814zm0 161.961v-71.431h45.602c32.661 0 49.608 12.03 49.608 35.49 0 23.459-16.484 35.941-47.605 35.941h-47.605z%27/%3E%3C/svg%3E');
+                    "></span>
+                `;
+
+                accordionButton.insertAdjacentHTML('afterbegin', iconHTML);
+            }
+        });
+
 		//hide useless bg color widget
 		$("#customize-control-background_color").hide();
 		
